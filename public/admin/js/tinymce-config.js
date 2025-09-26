85b8c2395a9c3a2e61364f1ba679a3e4781c2c98
Huy Nguyễn <nqhuy07072006@gmail.com>
2025-09-26 14:37:54 +0700

    update upload tinyMCE

--- public/admin/js/tinymce-config.js
+++ public/admin/js/tinymce-config.js
@@ -2,5 +2,29 @@ tinymce.init({
   selector: "textarea.textarea-tinyMCE",
   height: 300,
   plugins: 'lists link image code table',
-  license_key: 'gpl' //  quan trọng: báo cho TinyMCE biết bạn dùng bản miễn phí GPL
+  license_key: 'gpl',
+  file_picker_callback: function (cb, value, meta) {
+    var input = document.createElement('input');
+    input.setAttribute('type', 'file');
+    input.setAttribute('accept', 'image/*');
+
+    input.onchange = function () {
+      var file = this.files[0];
+      var reader = new FileReader();
+
+      reader.onload = function () {
+        var id = 'blobid' + (new Date()).getTime();
+        var blobCache = tinymce.activeEditor.editorUpload.blobCache;
+        var base64 = reader.result.split(',')[1];
+        var blobInfo = blobCache.create(id, file, base64);
+        blobCache.add(blobInfo);
+
+        cb(blobInfo.blobUri(), { title: file.name });
+      };
+
+      reader.readAsDataURL(file); // ✅ đúng
+    };
+
+    input.click(); // ✅ quan trọng
+  }
 });
