1d316f97f7e2094b431769d39331f802403cda29
Huy Nguyễn <nqhuy07072006@gmail.com>
2025-10-06 13:55:09 +0700

    hiển thị menu danh mục

--- routes/client/index.route.js
+++ routes/client/index.route.js
@@ -1,7 +1,9 @@
+const categoryMiddleWare = require("../../middlewares/client/category.middeware");
 const productRoutes = require("./product.route");
 const homeRoutes = require("./home.route");
 
 module.exports = (app) => {
+  app.use(categoryMiddleWare.category)
   app.use("/", homeRoutes);
   app.use("/products", productRoutes);
 };
