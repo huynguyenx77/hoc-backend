763ebbbb8f803c8778bf9cd2d5ce45b49beac4d9
Huy Nguyễn <nqhuy07072006@gmail.com>
2025-10-06 21:38:38 +0700

    lấy trang danh sách sp bên client

--- controllers/client/home.controller.js
+++ controllers/client/home.controller.js
@@ -2,15 +2,29 @@ const Product = require("../../models/product.models")
 const productHelper = require("../../helpers/product")
 //* [GET] /
 module.exports.index = async (req, res) => {
+  // * lấy ra sản phẩm nổi bật
   const productsFeatured = await Product.find({
     featured: "1",
     deleted: false,
     status: "active"
   }).limit(6);
   
-  const newProducts = productHelper.priceNewProducts(productsFeatured);
+  const newProductsFeatured = productHelper.priceNewProducts(productsFeatured);
+  // *  hết lấy ra sản phẩm nổi bật
+
+  // *lấy ra sản phẩm mới nhất
+  const productsNew = await Product.find({
+    deleted: false,
+    status: "active"
+  }).sort({ position : "desc"}).limit(6)
+
+  const newProductsNew = productHelper.priceNewProducts(productsNew);
+  // *hết lấy ra sản phẩm mới nhất
+
+
   res.render("client/pages/home/index", {
     pageTitle: "Trang chủ",
-    productsFeatured: newProducts
+    productsFeatured: newProductsFeatured,
+    productsNew: newProductsNew,
   });
 };
--- controllers/client/product.controller.js
+++ controllers/client/product.controller.js
@@ -1,13 +1,15 @@
 const Product = require("../../models/product.models");
+const ProductCategory = require("../../models/product-category.models");
 
-const productHelper = require("../../helpers/product")
+const productHelper = require("../../helpers/product");
+const productCategoryHelper = require("../../helpers/product-category");
 //* [GET] /products
 module.exports.index = async (req, res) => {
   const products = await Product.find({
     status: "active",
     deleted: false,
   }).sort({ position: "desc" });
-  
+
   const newProducts = productHelper.priceNewProducts(products);
   res.render("client/pages/products/index", {
     pageTitle: "Trang danh sách sản phẩm",
@@ -32,4 +34,28 @@ module.exports.detail = async (req, res) => {
   } catch (error) {
     res.redirect(`/products`);
   }
+};
+
+//* [GET] /products/:slugCategory
+module.exports.category = async (req, res) => {
+  const category = await ProductCategory.findOne({
+    slug: req.params.slugCategory,
+    status: "active",
+    deleted: false,
+  });
+
+  const listSubCategory = await productCategoryHelper.setSubCategory(category.id);
+
+  const listSubCategoryId = listSubCategory.map(item => item.id);
+
+  const products = await Product.find({
+    products_category_id: { $in: [category.id, ...listSubCategoryId] },
+    deleted: false,
+  }).sort({ position: "desc" });
+
+  const newProducts = productHelper.priceNewProducts(products);
+  res.render("client/pages/products/index", {
+    pageTitle: category.title,
+    products: newProducts,
+  });
 };
