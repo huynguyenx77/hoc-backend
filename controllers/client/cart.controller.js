557c768a11d804b86a7e6e44d42cc4389bc5c49a
Huy Nguyễn <nqhuy07072006@gmail.com>
2025-10-10 11:29:23 +0700

    làm trang giỏ hàng

--- controllers/client/cart.controller.js
+++ controllers/client/cart.controller.js
@@ -1,4 +1,32 @@
 const Cart = require("../../models/cart.model")
+const Product = require("../../models/product.models")
+
+
+const priceNewHelper = require("../../helpers/product");
+//* [GET]/
+module.exports.index = async (req, res) => {
+  const cartId =  req.cookies.cartId;
+  const cart = await Cart.findOne({
+    _id: cartId,
+  })
+  if(cart.products.length > 0){
+    for(const item of cart.products){
+      const productId = item.product_id;
+      const productInfo = await Product.findOne({
+        _id: productId,
+      });
+      productInfo.priceNew = priceNewHelper.priceNewProduct(productInfo)
+      item.productInfo = productInfo;
+      item.totalPrice = item.quantity * productInfo.priceNew
+    }
+  }
+  cart.totalPrice = cart.products.reduce((sum, item) => sum + item.totalPrice, 0);
+  res.render("client/pages/cart/index", {
+    pageTitle: "Giỏ hàng",
+    cartDetail: cart
+  });
+};
+
 //* [POST] /cart/add
 module.exports.addPost = async (req, res) => {
   const cartId = req.cookies.cartId;
