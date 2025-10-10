6c7f2d266797027909bb1a13dfa57acc4ba547c0
Huy Nguyễn <nqhuy07072006@gmail.com>
2025-10-10 14:06:21 +0700

    xóa sản phẩm khỏi giỏ hàng

--- controllers/client/cart.controller.js
+++ controllers/client/cart.controller.js
@@ -1,68 +1,89 @@
-const Cart = require("../../models/cart.model")
-const Product = require("../../models/product.models")
-
+const Cart = require("../../models/cart.model");
+const Product = require("../../models/product.models");
 
 const priceNewHelper = require("../../helpers/product");
 //* [GET]/
 module.exports.index = async (req, res) => {
-  const cartId =  req.cookies.cartId;
+  const cartId = req.cookies.cartId;
   const cart = await Cart.findOne({
     _id: cartId,
-  })
-  if(cart.products.length > 0){
-    for(const item of cart.products){
+  });
+  if (cart.products.length > 0) {
+    for (const item of cart.products) {
       const productId = item.product_id;
       const productInfo = await Product.findOne({
         _id: productId,
       });
-      productInfo.priceNew = priceNewHelper.priceNewProduct(productInfo)
+      productInfo.priceNew = priceNewHelper.priceNewProduct(productInfo);
       item.productInfo = productInfo;
-      item.totalPrice = item.quantity * productInfo.priceNew
+      item.totalPrice = item.quantity * productInfo.priceNew;
     }
   }
-  cart.totalPrice = cart.products.reduce((sum, item) => sum + item.totalPrice, 0);
+  cart.totalPrice = cart.products.reduce(
+    (sum, item) => sum + item.totalPrice,
+    0
+  );
   res.render("client/pages/cart/index", {
     pageTitle: "Giỏ hàng",
-    cartDetail: cart
+    cartDetail: cart,
   });
 };
 
-//* [POST] /cart/add
+//* [POST] /cart/add/:productId
 module.exports.addPost = async (req, res) => {
   const cartId = req.cookies.cartId;
   const productId = req.params.productId;
   const quantity = parseInt(req.body.quantity);
   const cart = await Cart.findOne({
     _id: cartId,
-  })
-  const existProductInCart = cart.products.find(item => item.product_id == productId);
+  });
+  const existProductInCart = cart.products.find(
+    (item) => item.product_id == productId
+  );
 
-  if(existProductInCart){
+  if (existProductInCart) {
     const newQuantity = quantity + existProductInCart.quantity;
     await Cart.updateOne(
       {
         _id: cartId,
-        'products.product_id': productId,      
+        "products.product_id": productId,
       },
       {
-        'products.$.quantity': newQuantity
+        "products.$.quantity": newQuantity,
       }
-    ) //* update value quantity in array of objects 
+    ); //* update value quantity in array of objects
   } else {
     const objectCart = {
       product_id: productId,
       quantity: quantity,
     };
-    
+
     await Cart.updateOne(
       {
-      _id: cartId,
+        _id: cartId,
       },
       {
-        $push: {products: objectCart}
+        $push: { products: objectCart },
       }
-    )
+    );
     req.flash("success", "Thêm sản phẩm vào giỏ hàng thành công!");
   }
+  res.redirect(req.get("Referrer"));
+};
+
+//* [GET] /delete/:id
+module.exports.delete = async (req, res) => {
+  const cartId = req.cookies.cartId;
+  const productId = req.params.productId;
+  await Cart.updateOne(
+    {
+      _id: cartId,
+    },
+    {
+      $pull: { products: { product_id: productId } },
+    }
+  );
+  console.log(productId);
+  req.flash("success", "Đã xóa sản phẩm thành công");
   res.redirect(req.get("Referrer"));
 };
