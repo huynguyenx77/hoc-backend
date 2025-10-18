3072aec64d67bfd83eaf5556ea87a99c9dce15dd
Huy Nguyễn <nqhuy07072006@gmail.com>
2025-10-19 00:33:25 +0700

    tính năng chat cơ bản

--- controllers/client/chat.controller.js
+++ controllers/client/chat.controller.js
@@ -3,18 +3,27 @@ const User = require("../../models/user.models");
 //* [GET] /search
 module.exports.index = async (req, res) => {
   const userId = res.locals.user.id;
+  const fullName = res.locals.user.fullName;
   // socketIO
   _io.once("connection", (socket) => {
     socket.on("CLIENT_SEND_MESSAGE", async (content) => {
+       // * Lưu vào database
       const chat = new Chat({
         user_id: userId,
         content: content,
       });
       await chat.save();
+      //* Trả data về client
+      _io.emit("SERVER_RETURN_MESSAGE", {
+        userId: userId,
+        fullName: fullName,
+        content: content,
+      });
     });
   });
   // end socketIO
 
+  // Lấy ra data
   const chats = await Chat.find({
     deleted: false,
   });
--- public/js/chat.js
+++ public/js/chat.js
@@ -1,14 +1,34 @@
 //* CLIENT_SEND_MESSAGE
 const formSendData = document.querySelector(".chat-box .inner-form");
 if (formSendData) {
-  formSendData.addEventListener("submit", (e) =>{
+  formSendData.addEventListener("submit", (e) => {
     e.preventDefault();
     const content = e.target.elements.content.value;
 
-    if(content) {
+    if (content) {
       socket.emit("CLIENT_SEND_MESSAGE", content);
-      e.target.elements.content.value = ""
+      e.target.elements.content.value = "";
     }
-  })
+  });
 }
 //* end CLIENT_SEND_MESSAGE
+
+//* SERVER_RETURN_MESSAGE
+socket.on("SERVER_RETURN_MESSAGE", (data) => {
+  const body = document.querySelector(".chat-box .inner-body");
+  const myId = document.querySelector("[my-id]").getAttribute("my-id");
+  let htmlFullName = "";
+  const div = document.createElement("div");
+  if (myId == data.userId) {
+    div.classList.add("inner-outgoing");
+  } else {
+    div.classList.add("inner-incoming");
+    htmlFullName = `<div class="inner-name">${data.fullName}</div>`;
+  }
+  div.innerHTML = `
+    ${htmlFullName}
+    <div class="inner-content">${data.content}</div>
+  `;
+  body.appendChild(div);
+});
+//* end SERVER_RETURN_MESSAGE
