4379a63a33e8438d7b6ed0977856cf6f391d39ee
Huy Nguyễn <nqhuy07072006@gmail.com>
2025-10-21 00:47:39 +0700

    chèn nhiều ảnh fix

---
+++ sockets/client/chat.socket.js
@@ -0,0 +1,37 @@
+const Chat = require("../../models/chat.model");
+const upload = require("../../helpers/uploadCloudinary");
+
+module.exports.chat = async (res) => {
+  const userId = res.locals.user.id;
+  const fullName = res.locals.user.fullName;
+  _io.once("connection", (socket) => {
+    socket.on("CLIENT_SEND_MESSAGE", async (data) => {
+      let images = [];
+      for (const imageBuffer of data.images) {
+        const link = await upload.upload(imageBuffer);
+        images.push(link);
+      }
+      // * Lưu vào database
+      const chat = new Chat({
+        user_id: userId,
+        content: data.content,
+        images: images,
+      });
+      await chat.save();
+      //* Trả data về client
+      _io.emit("SERVER_RETURN_MESSAGE", {
+        userId: userId,
+        fullName: fullName,
+        content: data.content,
+        images: images,
+      });
+    });
+    socket.on("CLIENT_SEND_TYPING", (type) => {
+      socket.broadcast.emit("SERVER_RETURN_TYPING", {
+        userId: userId,
+        fullName: fullName,
+        type: type,
+      });
+    });
+  });
+};
